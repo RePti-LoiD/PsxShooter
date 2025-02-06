22cbe83899e3229e75950fa615581083628ef700
Сергей Бельчиков <delhikovsr@gmail.com>
2025-02-06 16:41:34 +0300

    Добавлена тряска оружия

--- Assets/Scripts/Weapons/Effects/ProceduralAnims/WeaponShake.cs
+++ Assets/Scripts/Weapons/Effects/ProceduralAnims/WeaponShake.cs
@@ -4,13 +4,28 @@ public class WeaponShake : MonoBehaviour
 {
     [SerializeField] private float shakeAmount;
     [SerializeField] private float shakeFreq;
+    [SerializeField] private float inputWeight;
+    [SerializeField] private float lerpValue;
 
-    private float Cos { get => Mathf.Cos(shakeFreq * Time.time) * shakeAmount; }
-    private float Sin { get => Mathf.Sin(shakeFreq * Time.time) * shakeAmount; }
+    private float time;
+    private float Cos { get => Mathf.Abs(Mathf.Cos(shakeFreq * time)) * shakeAmount; }
+    private float Sin { get => Mathf.Sin(shakeFreq * time) * shakeAmount; }
 
     
     public void OnMove(Vector2 input)
     {
-        transform.localPosition = input;
+        input = -input;
+        if (input != Vector2.zero)
+            time += Time.deltaTime;
+
+        transform.localPosition = Vector3.Lerp (
+            transform.localPosition, 
+            new Vector3 (
+                Sin + input.x * inputWeight, 
+                Cos, 
+                input.y * inputWeight
+            ), 
+            Time.deltaTime * lerpValue
+        );
     }
 }
